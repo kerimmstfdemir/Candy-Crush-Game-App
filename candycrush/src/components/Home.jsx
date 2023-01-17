e985f72de07d8fa1905c087a2706b39e3ced6028
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-17 16:42:22 +0300

    added logo and updated home page

--- candycrush/src/components/Home.jsx
+++ candycrush/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { UserContext } from "../context/context"
 import { useNavigate } from "react-router-dom"
+import candycrush from "../assests/candy-crush.png"
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const Home = () => {
@@ -19,9 +20,10 @@ const Home = () => {
 
     console.log(userInfos);
     return (
-        <div className="h-screen flex flex-row justify-center items-center">
+        <div className="h-screen flex flex-col justify-center items-center gap-8">
+            <img className="w-[18.5rem]" src={candycrush} alt="candycrush" />
             <div className="w-full h-[30rem] max-w-2xl max-h-lg ">
-                <form className="flex flex-col gap-3 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <form className="flex flex-col gap-3 bg-white drop-shadow-[10px_-8px_46px_rgba(0,0,0,0.25)] rounded px-8 pt-6 pb-8 mb-4">
                     <div className="flex flex-col gap-2 mb-2">
                         <label
                             className="block text-[1.5rem] text-gray-700 text-sm font-bold mb-2"
@@ -39,7 +41,7 @@ const Home = () => {
                     </div>
                     <div className="flex items-center justify-center">
                         <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            className="text-[1.2rem] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
                             type="button" onClick={handleClick}>
                             Continue
                         </button>
