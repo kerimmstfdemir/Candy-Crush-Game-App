9a70ba0378092334a3e66aa0319bbd86f5139625
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-17 17:19:36 +0300

    created and modified playground area

--- candycrush/src/components/GameBoard.jsx
+++ candycrush/src/components/GameBoard.jsx
@@ -1,6 +1,50 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
+
 const GameBoard = () => {
+  
+  const [colorsArr, setColorsArr] = useState([]) 
+  
+  const width = 8
+  const candyCrushColors = [
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "red",
+    "yellow"
+  ]
+
+  const createBoard = () => {
+    const randomColors = []
+    for (let i = 0; i < width*width; i++){
+      const randomColor = candyCrushColors[Math.floor(Math.random() * candyCrushColors.length)]
+      randomColors.push(randomColor)
+    }
+    setColorsArr(randomColors)
+  }
+
+  useEffect(() => {
+    createBoard();
+  }, [])
+
+  console.log(colorsArr)
+
   return (
-    <div>GameBoard</div>
+    <div className="flex justify-center p-12">
+    <div className="w-[35rem] h-[35rem] flex flex-wrap">
+      {colorsArr.map((item, index) => {
+        return(
+          <>
+          <img
+          className="w-[4.375rem] h-[4.375rem]"
+          alt={item} 
+          style={{ backgroundColor:item }} />
+          </>
+        )
+      }) }
+    </div>
+    </div>
   )
 }
 
